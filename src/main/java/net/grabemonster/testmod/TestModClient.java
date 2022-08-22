05d86d4a202c0d010c9c09cd17ac3a22da0fb63b
BuildTools <unconfigured@null.spigotmc.org>
2022-08-22 14:50:41 +0200

    Initial commit

---
+++ src/main/java/net/grabemonster/testmod/TestModClient.java
@@ -0,0 +1,10 @@
+package net.grabemonster.testmod;
+
+import net.fabricmc.api.ClientModInitializer;
+//comment
+public class TestModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
